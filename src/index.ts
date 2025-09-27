91fcc678f307b7a0333738d6e6cca5114b009d6a
lunarash0 <eamonhwatson@icloud.com>
2025-09-27 23:30:42 +0200

    add console log for port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import WebSocket, { WebSocketServer } from 'ws';
 
+let port = 8080;
+
 const ws = new WebSocketServer({
-  port: 8080,
+  port: port,
   host: "0.0.0.0"
 });
+
+console.log("WebSocket server started on port:", port);
+console.log("Visit this website at: http://localhost:" + port);
